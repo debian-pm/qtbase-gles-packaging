e6f14c992d02ab66e465389bfc699e4398998b1a
Sune Vuorela <sune@vuorela.dk>
2013-05-18 22:05:37 +0200

    Create a script to mark symbols as private.

---
+++ debian/mark_private_symbols.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+PRIVATE_HEADERS=qtbase5-private-dev/usr/include
+
+error() {
+	echo $@
+	exit 1
+}
+
+debug() {
+	[ -n "${DEBUG}" ] && echo $@
+}
+
+if [ ! -e "${PRIVATE_HEADERS}" ] 
+then
+	error "Private headers not found"
+fi
+
+grep -rh class qtbase5-private-dev/usr/include | 
+	grep EXPORT | 
+	while read class export classname rest 
+	do
+		echo ${#classname}${classname} 
+	done | 
+	while read privateclass 
+	do
+		debug marking ${privateclass} as private
+		sed -i "s/\(.*${privateclass}[^ ]* *[^ ]*\)$/\1 1/" *.symbols 
+	done 
+
